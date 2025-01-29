0cc19c68bca14452c75d4cb6ccab837daab2bbf1
Josh-Alhassan <saintjosh26@gmail.com>
2025-01-29 04:07:27 +0100

    refactor api services

--- src/app/(dashboard)/dashboard/(admin)/admin-management/Tabs/admins/adminService.ts
+++ src/app/(dashboard)/dashboard/(admin)/admin-management/Tabs/admins/adminService.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-// Define the interface for the admin data
+// Admin data Interface
 export interface AdminData {
   id: number;
   name: string;
@@ -10,7 +10,7 @@ export interface AdminData {
   status: boolean;
 }
 
-// Define the base URL for your API
+// API base URL API
 const API_BASE_URL = "http://127.0.0.1:8000/api/v1/";
 
 // Function to fetch admin data
@@ -20,6 +20,6 @@ export const getAdmins = async (): Promise<AdminData[]> => {
     return response.data; // Return the fetched data
   } catch (error) {
     console.error("Error fetching admin data:", error);
-    throw error; // Handle the error appropriately
+    throw error; 
   }
 };
