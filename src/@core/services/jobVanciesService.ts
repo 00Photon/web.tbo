ce0ad5d9481b635a87792067492189a5498294ad
Josh-Alhassan <saintjosh26@gmail.com>
2025-01-30 22:02:13 +0100

    fix job vacancy api end point

--- src/@core/services/jobVanciesService.ts
+++ src/@core/services/jobVanciesService.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+import { getSession } from "next-auth/react";
+
 export interface Job {
   id: number;
   title: string;
@@ -26,16 +28,32 @@ interface ApiResponse {
 }
 
 // Base URL for the API
-const API_BASE_URL = "https://api.tbo-taas.com/api/v1/jobs";
+const API_BASE_URL = "https://api.tbo-taas.com/api/v1";
 console.log(API_BASE_URL);
 
 export const getJobs = async (): Promise<Job[]> => {
   try {
-    const response = await axios.get<ApiResponse>(`${API_BASE_URL}/jobs`);
-    console.log(response.data.jobs);
+    const session = await getSession(); // Get the session, which includes the token
+    if (!session || !session.user) throw new Error("User is not authenticated");
+
+    const token = session.user.accessToken;
+
+    const response = await axios.get<ApiResponse>(`${API_BASE_URL}/jobs`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`, // Add the token to the Authorization header
+        "Content-Type": "application/json",
+      },
+    });
+    console.log(response.data.status);
     return response.data.jobs;
   } catch (error) {
-    console.error("Error fetching jobs:", error);
+    if (axios.isAxiosError(error)) {
+      console.error("Axios Error:", error.response?.data);
+    } else {
+      console.error("Unexpected Error:", error);
+    }
+    // console.error("Error fetching jobs:", error);
     throw error;
   }
 };
